bc8c823bd883db76976aa0a6bc1dcf1f5733dea3
Trevor Brown <trevorbrown25@gmail.com>
2018-09-12 21:12:22 -0500

    Added build thingy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use( express.static( `${__dirname}/../build` ) );
+app.use( express.static( `build` ) );
 
 const port = 3333
 app.listen(port, () => console.log(`listening on port ${port}`));
