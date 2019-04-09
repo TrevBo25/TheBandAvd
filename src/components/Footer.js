fe9c3457c7872b3eaf7bf3d2854f24719cca1745
Trevor Brown <trevorbrown25@gmail.com>
2019-04-09 13:09:47 -0500

    Added in booking information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,6 +15,22 @@ export default class Footer extends Component {
                         <a href="https://www.youtube.com/user/thebandavondale" target="_blank" rel="noopener noreferrer" ><i className="smicon fab fa-youtube"></i></a>
                         <a href="https://open.spotify.com/artist/1s6Fgx0rDx46sATrAURbq5" target="_blank" rel="noopener noreferrer" ><i className="smicon fab fa-spotify"></i></a>
                     </div>
+                    <div className="booking">
+                        <div className="al-booking">
+                            <p className="booking-title">Booking information:</p>
+                            <p className="booking-text">Nightclubs and restaurants</p>
+                            <p className="booking-text">Alec Heist</p>
+                            <p className="booking-text">(309) 781-4324</p>
+                        </div>
+                        <div className="al-booking">
+                            <p className="booking-title">All other booking inquiries:</p>
+                            <p className="booking-text">Rollin Riggs</p>
+                            <p className="booking-text">Riggs Entertainment LLC</p>
+                            <p className="booking-text">(901) 341-6565</p>
+                            <p className="booking-text">rollin@riggsentertainmentllc.com</p>
+                            <a href="https://riggsentertainmentllc.com/entertainers/avon-dale" target="_blank" rel="noopener noreferrer"><span className="booking-text underline">Avon Dale on Riggs Entertainment!</span></a>
+                        </div>
+                    </div>
                 </div>
                 <div className="emailholder">
                     <h2 className="emailtitle">Don't Miss Out!</h2>
